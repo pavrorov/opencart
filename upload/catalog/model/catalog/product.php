f599ab98c20197f5aacb28f99d9ffb3135b50ac4
Pavel Avrorov <avrorov@priboy.online>
2019-04-17 00:34:02 +0300

    Add argument 'age' (in days) to getLatestProducts()
    Now it is possible to query for products added not earlier the given
    age --- i.e. asking for really *new* products, if any.
    
    Signed-off-by: Pavel Avrorov <avrorov@priboy.online>

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -256,17 +256,21 @@ class ModelCatalogProduct extends Model {
 		return $product_data;
 	}
 
-	public function getLatestProducts($limit) {
-		$product_data = $this->cache->get('product.latest.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit);
+	public function getLatestProducts($limit, $age) {
+		$product_data = $this->cache->get('product.latest.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit . (isset($age) ? '.'.$age : ''));
 
 		if (!$product_data) {
-			$query = $this->db->query("SELECT p.product_id FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) WHERE p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "' ORDER BY p.date_added DESC LIMIT " . (int)$limit);
+			$ageclause = "";
+			if (isset($age)) {
+				$ageclause = "AND DATEDIFF(NOW(), p.date_added) < $age";
+			}
+			$query = $this->db->query("SELECT p.product_id FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) WHERE p.status = '1' $ageclause AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "' ORDER BY p.date_added DESC LIMIT " . (int)$limit);
 
 			foreach ($query->rows as $result) {
 				$product_data[$result['product_id']] = $this->getProduct($result['product_id']);
 			}
 
-			$this->cache->set('product.latest.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit, $product_data);
+			$this->cache->set('product.latest.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit . (isset($age) ? '.'.$age : ''), $product_data);
 		}
 
 		return $product_data;
